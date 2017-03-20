c2a8635d79bd9fe385892bb41725dd375b27c954
Alex Moores <moores.alexd@gmail.com>
2017-03-20 19:00:10 -0400

    added basic queue and queue element components closes #22

--- client/src/js/player.jsx
+++ client/src/js/player.jsx
@@ -79,29 +79,124 @@ class Add extends React.Component {
   }
 };
 
-class Queue extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <p>This is the Queue</p>
-    )
-  }
-};
-
 class QueueElement extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      upVote: this.props.video.upVote,
+      downVote: this.props.video.downVote,
+      downVoted: false,
+      upVoted: false,
+      downStyle: {},
+      upStyle: {}
+    };
+  }
+
+  voteUp() {
+    if(this.state.upVoted) {
+      this.setState({
+        upVote: this.state.upVote - 1,
+        upVoted: false,
+        upStyle: {}
+      });
+    } else if (this.state.downVoted) {
+      this.setState({
+        upVote: this.state.upVote + 1,
+        upVoted: true,
+        upStyle: {border: '1px solid green'},
+        downVote: this.state.downVote - 1,
+        downVoted: false,
+        downStyle: {}
+      });
+    } else {
+      this.setState({
+        upVote: this.state.upVote + 1,
+        upVoted: true,
+        upStyle: {border: '1px solid green'}
+      });
+    }
+  }
+
+  voteDown() {
+    if(this.state.downVoted) {
+      this.setState({
+        downVote: this.state.downVote - 1,
+        downVoted: false,
+        downStyle: {}
+      });
+    } else if (this.state.upVoted) {
+      this.setState({
+        downVote: this.state.downVote + 1,
+        downVoted: true,
+        downStyle:  {border: '1px solid red'},
+        upVote: this.state.upVote - 1,
+        upVoted: false,
+        upStyle: {}
+      });
+    } else {
+      this.setState({
+        downVote: this.state.downVote + 1,
+        downVoted: true,
+        downStyle: {border: '1px solid red'}
+      });
+    }
   }
 
   render() {
     return (
-      <p>This is the QueueElement</p>
+      <table>
+        <tr>
+          <td>
+            <p> {this.props.video.title} </p>
+            <p> {this.props.video.videoUrl} </p>
+          </td>
+
+          <td>
+            <button onClick={this.voteUp.bind(this)} style={this.state.upStyle}> ^ </button>
+            <span> {this.state.upVote} </span>
+          </td>
+
+          <td>
+            <button onClick={this.voteDown.bind(this)} style={this.state.downStyle}> v </button>
+            <span> {this.state.downVote} </span>
+          </td>
+        </tr>
+      </table>
     )
   }
 };
+
+class Queue extends React.Component {
+  constructor(props) {
+    super(props);
+
+    var dummyData = [
+      {id: 0, videoUrl: 'https://www.youtube.com/watch?v=laNCDelVXpk', title: 'Lucian ft. Olivera - Sober Heart', upVote: 10, downVote: 0},
+      {id: 1, videoUrl: 'https://www.youtube.com/watch?v=A8jXapCG0VQ', title: 'Futuristik ft. Miyoki - Waterborne', upVote: 3, downVote: 3},
+      {id: 2, videoUrl: 'https://www.youtube.com/watch?v=PZbkF-15ObM', title: 'C2C - Delta (official Video)', upVote: 284, downVote: 23},
+      {id: 3, videoUrl: 'https://www.youtube.com/watch?v=Wga5A6R9BJg', title: 'Slightly Left of Centre - Love The Way You Move (LTWYM) Official Music Video', upVote: 4, downVote: 8}
+    ];
+
+    this.state = {
+      videoList: dummyData
+    };
+  }
+
+  render() {
+    var queueElements = [];
+    _.each(this.state.videoList, function(video) {
+      queueElements.push(<QueueElement video={video} key={video.id}/>);
+    });
+
+    return (
+      <div>
+        { queueElements }
+      </div>
+    );
+  }
+};
+
 
 class Chat extends React.Component {
   constructor(props) {
