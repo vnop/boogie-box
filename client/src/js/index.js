4623e334875bf7d379c31fcafc9c927d27d43efd
Alex Moores <moores.alexd@gmail.com>
2017-03-21 17:31:09 -0400

    fixed bower issues

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,4 +1,4 @@
-var socket = io('http://localhost:8082');
+var socket = io('http://localhost:8081');
 var adminFlag = false;
 
 ReactDOM.render(<App socket={socket} adminFlag={adminFlag} />, document.getElementById('app'));
